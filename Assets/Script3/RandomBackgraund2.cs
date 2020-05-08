53d2cdcd137c0b9795b63f0915a9233be1c902bb
MakMaka <62114682+MakMaka@users.noreply.github.com>
2020-05-08 15:53:02 +0300

    2 демо уровня
    .

---
+++ Assets/Script3/RandomBackgraund2.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class RandomBackgraund2 : MonoBehaviour
+{
+    public Randomchik ran;
+    public Statistics2 sts2;
+
+    public GameObject canvas;
+    public Slider sl;
+    int popitok = 6;
+
+    private bool nextLevel;
+
+    public Sprite colDown, colUp;
+
+    int textCheck;
+
+    int i = 0;
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = colDown;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().sprite = colUp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Advertisement.isSupported)
+        {
+            Advertisement.Initialize("e979571e-3da3-4da9-8847-56e67f8c6042", false);
+        }
+
+        if (PlayerPrefs.GetString("Music") == "no")
+        {
+            GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+        }
+        else
+        {
+            GameObject.Find("NatureAudio").GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (PlayerPrefs.GetString("Music") != "no")
+            GameObject.Find("ClickAudio").GetComponent<AudioSource>().Play();
+        switch (gameObject.name)
+        {
+            case "Okey_Button":
+                CheckStatistic();
+                break;
+            case "Cancel_Button":
+                CheckStatistic2();
+                break;
+        }
+    }
+
+    public void CheckStatistic()
+    {
+        if (ran.type == sts2.Type())
+        {
+            textCheck = int.Parse(ran.animalTorso2.text);
+            if (textCheck > 51)
+            {
+                textCheck = int.Parse(ran.animalLegs2.text);
+                if(textCheck < 50)
+                {
+                    if (sl.value < popitok)
+                    {
+                        GetComponent<AudioSource>().Play();
+                        ran.onRandomchik = true;
+                        sl.value = sl.value + 1;
+                    }
+                    if (sl.value == 5)
+                    {
+                        GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+                        Application.LoadLevel("Game");
+                    }
+                }
+                else
+                {
+                    GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+                    canvas.SetActive(true);
+                    if (Advertisement.IsReady())
+                    {
+                        Advertisement.Show();
+                    }
+                }
+                    
+            }
+            else
+            {
+                GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+                canvas.SetActive(true);
+                if (Advertisement.IsReady())
+                {
+                    Advertisement.Show();
+                }
+            }
+                
+        }
+        else
+        if (ran.type != sts2.Type())
+        {
+            GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+            canvas.SetActive(true);
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show();
+            }
+        }
+    }
+
+    public void CheckStatistic2()
+    {
+        if (ran.type == sts2.Type())
+        {
+            textCheck = int.Parse(ran.animalTorso2.text);
+            if (textCheck > 50)
+            {
+                textCheck = int.Parse(ran.animalLegs2.text);
+                if (textCheck < 50)
+                {
+                    GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
+                    canvas.SetActive(true);
+                    if (Advertisement.IsReady())
+                    {
+                        Advertisement.Show();
+                    }
+                }
+                else
+                    ran.onRandomchik = true;
+            }
+            else
+                ran.onRandomchik = true;
+        }
+        else
+        if (ran.type != sts2.Type())
+        {
+            ran.onRandomchik = true;
+        }
+
+    }
+}
